317b4f5178be7e603e1c416f0ed01eb91413632e
shohruh223 <shohruh.abd2223@gmail.com>
2025-07-11 21:56:19 +0500

    add

--- app/main.py
+++ app/main.py
@@ -1,39 +1,58 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from .database import Base, engine
+from .routes import create_product, create_category, shop
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="app/templates")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 Base.metadata.create_all(bind=engine)
 
-
-# from fastapi import FastAPI
-#
-# app = FastAPI()
-#
-#
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-#
-#
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     return {"message": f"Hello {name}"}
-
-# --------------------------------------------------
+app.include_router(create_category.router)
+app.include_router(create_product.router)
+app.include_router(shop.router)
 
 
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    context = {"request": request, "name": "Alijon"}
-    return templates.TemplateResponse("index.html", context)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get("/about", response_class=HTMLResponse)
-def home(request: Request):
-    context = {"request": request, "about": "Karatash neighbourhood"}
-    return templates.TemplateResponse("about.html", context)
+@app.get("/testimonial", response_class=HTMLResponse)
+async def testimonial(request: Request):
+    return templates.TemplateResponse("testimonial.html", {"request": request})
+
+
+@app.get("/contact", response_class=HTMLResponse)
+async def contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request})
+
+
+@app.get("/checkout", response_class=HTMLResponse)
+async def checkout(request: Request):
+    return templates.TemplateResponse("checkout.html", {"request": request})
+
+
+@app.get("/cart", response_class=HTMLResponse)
+async def cart(request: Request):
+    return templates.TemplateResponse("cart.html", {"request": request})
+
+
+@app.get("/404", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("404.html", {"request": request})
+
+
+# register
+@app.get("/register", response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("auth/register.html", {"request": request})
+
+
+@app.get("/login", response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("auth/login.html", {"request": request})
--- app/models.py
+++ app/models.py
@@ -1,9 +1,36 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, CheckConstraint, Text
+from sqlalchemy.orm import relationship
 from .database import Base
 
 
-class User(Base):
-    __tablename__ = "users"
+class Category(Base):
+    __tablename__ = "categories"
+
     id = Column(Integer, primary_key=True, index=True)
-    image = Column(String, nullable=True)
-    name = Column(String, index=True)
+    title = Column(String, unique=True, nullable=False, index=True)
+
+    # One-to-many: Category → Products
+    products = relationship("Product", back_populates="category", cascade="all, delete")
+
+    def __str__(self):
+        return self.title
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    image = Column(String, nullable=False)
+    title = Column(String, nullable=False, index=True)
+
+    # ForeignKey to category table
+    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False, index=True)
+    category = relationship("Category", back_populates="products")  # assuming reverse relation
+
+    price = Column(Numeric(10, 2), nullable=False, index=True)  # Decimal: max 99999999.99
+    review = Column(Integer, CheckConstraint('review BETWEEN 1 AND 5'), nullable=True, index=True)
+    text = Column(Text, nullable=True)
+    quantity = Column(Integer, nullable=True, default=1, index=True)
+
+
+
