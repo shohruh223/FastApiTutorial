5da2095539d4d2e5e9d5bc681a94fcf4ab2e8efa
shox <shohruh.abd2223@gmail.com>
2025-09-29 16:03:31 +0500

    new

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# postgresql://username:password@host:port/database_name
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost:5432/testdb"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
